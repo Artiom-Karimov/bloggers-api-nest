d48a09a2f8213e04ccf6e01460786066012ed213
Artiom Karimov <lunatic0259@gmail.com>
2022-11-15 22:09:59 +0300

    updated test module

--- src/modules/test/test.module.ts
+++ src/modules/test/test.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import Recovery, {
+  RecoverySchema,
+} from '../auth/models/recovery/recovery.schema';
+import Session, { SessionSchema } from '../auth/models/session/session.schema';
 import Blog, { BlogSchema } from '../blogs/models/blog.schema';
-import Comment, { CommentSchema } from '../posts/models/comments/comment.schema';
+import Comment, {
+  CommentSchema,
+} from '../posts/models/comments/comment.schema';
+import {
+  CommentLike,
+  CommentLikeSchema,
+  PostLike,
+  PostLikeSchema,
+} from '../posts/models/likes/like.schema';
 import Post, { PostSchema } from '../posts/models/posts/post.schema';
+import UserBan, { UserBanSchema } from '../users/models/ban/user.ban.schema';
+import EmailConfirmation, {
+  EmailConfirmationSchema,
+} from '../users/models/email/email.confirmation.schema';
 import User, { UserSchema } from '../users/models/user.schema';
 import TestController from './test.controller';
 import TestRepository from './test.repository';
@@ -11,8 +27,23 @@ import TestRepository from './test.repository';
   imports: [
     MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: PostLike.name, schema: PostLikeSchema },
+    ]),
     MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+    MongooseModule.forFeature([
+      { name: CommentLike.name, schema: CommentLikeSchema },
+    ]),
+
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: EmailConfirmation.name, schema: EmailConfirmationSchema },
+    ]),
+    MongooseModule.forFeature([{ name: Session.name, schema: SessionSchema }]),
+    MongooseModule.forFeature([{ name: UserBan.name, schema: UserBanSchema }]),
+    MongooseModule.forFeature([
+      { name: Recovery.name, schema: RecoverySchema },
+    ]),
   ],
   controllers: [TestController],
   providers: [TestRepository],
--- src/modules/test/test.repository.ts
+++ src/modules/test/test.repository.ts
@@ -1,28 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import Recovery, {
+  RecoveryDocument,
+} from '../auth/models/recovery/recovery.schema';
+import Session, {
+  SessionDocument,
+} from '../auth/models/session/session.schema';
 import Blog, { BlogDocument } from '../blogs/models/blog.schema';
-import Comment, { CommentDocument } from '../posts/models/comments/comment.schema';
+import Comment, {
+  CommentDocument,
+} from '../posts/models/comments/comment.schema';
+import {
+  CommentLike,
+  LikeDocument,
+  PostLike,
+} from '../posts/models/likes/like.schema';
 import Post, { PostDocument } from '../posts/models/posts/post.schema';
+import UserBan, { UserBanDocument } from '../users/models/ban/user.ban.schema';
+import EmailConfirmation, {
+  EmailConfirmationDocument,
+} from '../users/models/email/email.confirmation.schema';
 import User, { UserDocument } from '../users/models/user.schema';
 
 @Injectable()
 export default class TestRepository {
   constructor(
-    @InjectModel(Blog.name) private readonly blogModel: Model<BlogDocument>,
-    @InjectModel(Post.name) private readonly postModel: Model<PostDocument>,
-    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
-    @InjectModel(Comment.name)
-    private readonly commentModel: Model<CommentDocument>,
+    @InjectModel(Blog.name) private readonly blog: Model<BlogDocument>,
+    @InjectModel(Post.name) private readonly post: Model<PostDocument>,
+    @InjectModel(PostLike.name) private readonly postLike: Model<LikeDocument>,
+
+    @InjectModel(Comment.name) private readonly comment: Model<CommentDocument>,
+    @InjectModel(CommentLike.name)
+    private readonly commentLike: Model<LikeDocument>,
+
+    @InjectModel(User.name) private readonly user: Model<UserDocument>,
+    @InjectModel(EmailConfirmation.name)
+    private readonly emailConfirmation: Model<EmailConfirmationDocument>,
+    @InjectModel(Session.name) private readonly session: Model<SessionDocument>,
+    @InjectModel(UserBan.name) private readonly userBan: Model<UserBanDocument>,
+    @InjectModel(Recovery.name)
+    private readonly recovery: Model<RecoveryDocument>,
   ) { }
 
   public dropAll = async () => {
     try {
       const promises = [
-        this.blogModel.deleteMany({}),
-        this.postModel.deleteMany({}),
-        this.userModel.deleteMany({}),
-        this.commentModel.deleteMany({}),
+        this.commentLike.deleteMany({}),
+        this.comment.deleteMany({}),
+        this.postLike.deleteMany({}),
+        this.post.deleteMany({}),
+        this.blog.deleteMany({}),
+
+        this.session.deleteMany({}),
+        this.recovery.deleteMany({}),
+        this.userBan.deleteMany({}),
+        this.emailConfirmation.deleteMany({}),
+        this.user.deleteMany({}),
       ];
       await Promise.all(promises);
     } catch (error) {
