dc69469df2fe336d829bfb83578c45101242b105
Artiom Karimov <lunatic0259@gmail.com>
2022-11-17 17:56:07 +0300

    refactoring

--- src/modules/blogs/blogs.module.ts
+++ src/modules/blogs/blogs.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { PostLike, PostLikeSchema } from '../posts/models/likes/like.schema';
-import Post, { PostSchema } from '../posts/models/posts/post.schema';
 import { PostsModule } from '../posts/posts.module';
 import BlogsController from './blogs.controller';
 import BlogsQueryRepository from './blogs.query.repository';
