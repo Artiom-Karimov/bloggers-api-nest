9fc5a46b07a84f5a54b5fafeb1f1fe59cd1478b4
Artiom Karimov <lunatic0259@gmail.com>
2022-11-21 14:15:07 +0300

    made optionalBearerGuard extension of bearerGuard

--- src/modules/auth/guards/optional.bearer.auth.guard.ts
+++ src/modules/auth/guards/optional.bearer.auth.guard.ts
@@ -1,24 +1,13 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Request } from 'express';
-import TokenPair from '../models/jwt/token.pair';
-import TokenPayload from '../models/jwt/token.payload';
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { BearerAuthGuard } from './bearer.auth.guard';
 
 @Injectable()
-export class OptionalBearerAuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    const request: Request = context.switchToHttp().getRequest();
-    if (!request.headers.authorization) return true;
-    request.body.tokenPayload = this.authorize(request.headers.authorization);
-
-    return true;
-  }
-  private authorize(authHeader: string): TokenPayload {
-    if (!authHeader.startsWith('Bearer ')) return undefined;
-
-    const token = authHeader.split(' ')[1];
-    const payload = TokenPair.unpackToken(token);
-    if (!payload) return undefined;
-
-    return payload;
+export class OptionalBearerAuthGuard extends BearerAuthGuard {
+  override canActivate(context: ExecutionContext): boolean {
+    try {
+      return super.canActivate(context);
+    } catch (error) {
+      return true;
+    }
   }
 }
