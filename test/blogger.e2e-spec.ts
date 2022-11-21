8d8ed5d6cc8e0e0428006825c0f56e3a3f930e6b
Artiom Karimov <lunatic0259@gmail.com>
2022-11-21 22:45:24 +0300

    added tests for blogger/blogs

--- test/blogger.e2e-spec.ts
+++ test/blogger.e2e-spec.ts
@@ -5,6 +5,8 @@ import PageViewModel from '../src/common/models/page.view.model';
 import BlogViewModel from '../src/modules/blogs/models/blog.view.model';
 import BlogSampleGenerator from './utils/blog.sample.generator';
 import UserSampleGenerator, { Tokens } from './utils/user.sample.generator';
+import { dateRegex } from '../src/common/utils/date.generator';
+import BlogInputModel from '../src/modules/blogs/models/blog.input.model';
 
 jest.useRealTimers();
 
@@ -13,6 +15,14 @@ describe('BloggerController (e2e)', () => {
   let app: INestApplication;
   let samples: BlogSampleGenerator;
   let userSamples: UserSampleGenerator;
+
+  const emptyPage = {
+    pagesCount: 0,
+    page: 1,
+    pageSize: expect.any(Number),
+    totalCount: 0,
+    items: [],
+  };
 
   beforeAll(async () => {
     app = await init();
@@ -54,13 +64,106 @@ describe('BloggerController (e2e)', () => {
       .get(`${base}`)
       .set('Authorization', `Bearer ${bloggerTokens.access}`);
     const body = result.body as PageViewModel<BlogViewModel>;
-    const emptyPage = {
-      pagesCount: 0,
+
+    expect(body).toEqual(emptyPage);
+  });
+
+  let sample: BlogInputModel;
+  let expectedBlog: any;
+  let createdBlog: BlogViewModel;
+
+  it('should be able to create blog', async () => {
+    sample = samples.generateSamples(1)[0];
+    expectedBlog = {
+      id: expect.any(String),
+      name: sample.name,
+      description: sample.description,
+      websiteUrl: sample.websiteUrl,
+      createdAt: expect.stringMatching(dateRegex),
+    };
+
+    const response = await request(app.getHttpServer())
+      .post(base)
+      .set('Authorization', `Bearer ${bloggerTokens.access}`)
+      .send(sample);
+
+    expect(response.body).toEqual(expectedBlog);
+    createdBlog = response.body;
+  });
+  it('should get created blog', async () => {
+    const response = await await request(app.getHttpServer())
+      .get(base)
+      .set('Authorization', `Bearer ${bloggerTokens.access}`);
+
+    const expected = {
+      pagesCount: 1,
       page: 1,
       pageSize: expect.any(Number),
-      totalCount: 0,
-      items: [],
+      totalCount: 1,
+      items: [expectedBlog],
     };
-    expect(body).toEqual(emptyPage);
+    expect(response.body).toEqual(expected);
+  });
+  it('should not get blogs', async () => {
+    const response = await request(app.getHttpServer())
+      .get(base)
+      .set('Authorization', `Bearer ${userTokens.access}`);
+
+    expect(response.body).toEqual(emptyPage);
+  });
+
+  let changedBlogSample: BlogInputModel;
+
+  it('should not change blog', async () => {
+    changedBlogSample = samples.generateSamples(1)[0];
+    const response = await request(app.getHttpServer())
+      .put(`${base}/${createdBlog.id}`)
+      .set('Authorization', `Bearer ${userTokens.access}`)
+      .send(changedBlogSample);
+
+    expect(response.statusCode).toBe(403);
+  });
+
+  it('should change blog', async () => {
+    let response = await request(app.getHttpServer())
+      .put(`${base}/${createdBlog.id}`)
+      .set('Authorization', `Bearer ${bloggerTokens.access}`)
+      .send(changedBlogSample);
+
+    expect(response.statusCode).toBe(204);
+
+    expectedBlog = {
+      id: createdBlog.id,
+      name: changedBlogSample.name,
+      description: changedBlogSample.description,
+      websiteUrl: changedBlogSample.websiteUrl,
+      createdAt: expect.stringMatching(dateRegex),
+    };
+
+    response = await request(app.getHttpServer())
+      .get(base)
+      .set('Authorization', `Bearer ${bloggerTokens.access}`);
+    const body = response.body as PageViewModel<BlogViewModel>;
+    expect(body.items[0]).toEqual(expectedBlog);
+  });
+  it('should not delete blog', async () => {
+    changedBlogSample = samples.generateSamples(1)[0];
+    const response = await request(app.getHttpServer())
+      .delete(`${base}/${createdBlog.id}`)
+      .set('Authorization', `Bearer ${userTokens.access}`);
+
+    expect(response.statusCode).toBe(403);
+  });
+  it('should delete blog', async () => {
+    let response = await request(app.getHttpServer())
+      .delete(`${base}/${createdBlog.id}`)
+      .set('Authorization', `Bearer ${bloggerTokens.access}`);
+    expect(response.statusCode).toBe(204);
+
+    response = await request(app.getHttpServer())
+      .get(base)
+      .set('Authorization', `Bearer ${bloggerTokens.access}`);
+
+    expect(response.body).toEqual(emptyPage);
   });
 });
