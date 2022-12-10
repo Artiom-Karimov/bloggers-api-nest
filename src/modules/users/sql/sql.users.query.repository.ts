3c4a07909bdee46b87dea22a3bff90779a1a9854
Artiom Karimov <lunatic0259@gmail.com>
2022-12-10 11:36:33 +0300

    added ban filter into sqlUsersQueryRepo

--- src/modules/users/sql/sql.users.query.repository.ts
+++ src/modules/users/sql/sql.users.query.repository.ts
@@ -3,7 +3,7 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import PageViewModel from '../../../common/models/page.view.model';
 import UsersQueryRepository from '../interfaces/users.query.repository';
-import GetUsersQuery from '../models/input/get.users.query';
+import GetUsersQuery, { BanStatus } from '../models/input/get.users.query';
 import SessionUserViewModel from '../models/view/session.user.view.model';
 import UserViewModel from '../models/view/user.view.model';
 import UserMapper from './models/mappers/user.mapper';
@@ -67,38 +67,58 @@ export default class SqlUsersQueryRepository extends UsersQueryRepository {
     );
   }
   private async getCount(params: GetUsersQuery): Promise<number> {
-    const filter = this.getFilter(
-      params.searchLoginTerm,
-      params.searchEmailTerm,
-    );
+    const filter = this.getCountFilter(params);
 
     const result = await this.db.query(
-      `select count(*) from "user" ${filter};`,
+      `select count(*) from "user" u
+      left join "userBan" b 
+      on u."id" = b."userId"
+      ${filter};`,
     );
     return +result[0]?.count ?? 0;
   }
-  private getFilter(
-    loginTerm: string | null,
-    emailTerm: string | null,
-  ): string {
-    const login = `lower("login") like '%${loginTerm?.toLowerCase()}%'`;
-    const email = `lower("email") like '%${emailTerm?.toLowerCase()}%'`;
-
-    if (loginTerm && emailTerm) {
-      return `where ${login} or ${email}`;
-    }
-    if (loginTerm) return `where ${login}`;
-    if (emailTerm) return `where ${email}`;
+  private getBanFilter(params: GetUsersQuery, includeWhere: boolean): string {
+    const { banStatus } = params;
+    const where = includeWhere ? 'where ' : '';
+    if (banStatus === BanStatus.Banned) return `${where}"isBanned" = True`;
+    if (banStatus === BanStatus.NotBanned)
+      return `${where}("isBanned" = False or "isBanned" is null)`;
     return '';
   }
+
+  private getUserFilter(params: GetUsersQuery, includeWhere: boolean): string {
+    const lt = params.searchLoginTerm;
+    const et = params.searchEmailTerm;
+    const where = includeWhere ? 'where ' : '';
+
+    const login = `lower("login") like '%${lt?.toLowerCase()}%'`;
+    const email = `lower("email") like '%${et?.toLowerCase()}%'`;
+
+    if (lt && et) {
+      return `${where}${login} or ${email}`;
+    }
+    if (lt) return `${where}${login}`;
+    if (et) return `${where}${email}`;
+    return '';
+  }
+
+  private getCountFilter(params: GetUsersQuery): string {
+    const userFilter = this.getUserFilter(params, false);
+    const banFilter = this.getBanFilter(params, false);
+
+    if (userFilter && banFilter)
+      return `where (${userFilter}) and ${banFilter}`;
+    if (userFilter) return `where ${userFilter}`;
+    if (banFilter) return `where ${banFilter}`;
+    return '';
+  }
+
   private async loadPageUsers(
     page: PageViewModel<UserViewModel>,
     params: GetUsersQuery,
   ): Promise<PageViewModel<UserViewModel>> {
-    const filter = this.getFilter(
-      params.searchLoginTerm,
-      params.searchEmailTerm,
-    );
+    const userFilter = this.getUserFilter(params, true);
+    const banFilter = this.getBanFilter(params, true);
     const order = params.sortDirection === 1 ? 'asc' : 'desc';
     const sortBy = this.transformSortBy(params.sortBy);
 
@@ -109,11 +129,12 @@ export default class SqlUsersQueryRepository extends UsersQueryRepository {
       from (
         select "id","login","email","hash","createdAt"
         from "user"
-        ${filter}
+        ${userFilter}
         order by ${sortBy} ${order}
         limit ${page.pageSize} offset ${page.calculateSkip()}
       ) as u left join "userBan" b
       on u."id" = b."userId"
+      ${banFilter}
       `,
     );
 
