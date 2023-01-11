d2508e77a1584a93c6126aa48a8e6194ab931689
Artiom Karimov <lunatic0259@gmail.com>
2023-01-11 12:00:12 +0300

    split adminController into users and blogs

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -3,10 +3,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AuthModule } from '../auth/auth.module';
 import { BlogsModule } from '../blogs/blogs.module';
 import { UsersModule } from '../users/users.module';
-import AdminController from './admin.controller';
 import { BanUserHandler } from './commands/handlers/ban.user.handler';
 import CreateConfirmedUserHandler from './commands/handlers/create.confirmed.user.handler';
 import DeleteUserHandler from './commands/handlers/delete.user.handler';
+import AdminBlogsController from './admin.blogs.controller';
+import AdminUsersController from './admin.users.controller';
 
 const commandHandlers = [
   BanUserHandler,
@@ -16,7 +17,7 @@ const commandHandlers = [
 
 @Module({
   imports: [CqrsModule, BlogsModule, UsersModule, AuthModule],
-  controllers: [AdminController],
+  controllers: [AdminBlogsController, AdminUsersController],
   providers: [...commandHandlers],
 })
 export class AdminModule { }
