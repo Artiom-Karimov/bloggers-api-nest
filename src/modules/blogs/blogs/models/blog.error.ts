1d353f588aaa3c3737e873bdbbf687c2e073432e
Artiom Karimov <lunatic0259@gmail.com>
2023-01-24 22:55:30 +0300

    bloggerUserController refactoring

--- src/modules/blogs/blogs/models/blog.error.ts
+++
@@ -1,6 +0,0 @@
-export enum BlogError {
-  NoError,
-  NotFound,
-  Forbidden,
-  Unknown,
-}
--- src/modules/blogs/blogs/usecases/handlers/blog.user.ban.handler.ts
+++ src/modules/blogs/blogs/usecases/handlers/blog.user.ban.handler.ts
@@ -2,12 +2,16 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import CommentsRepository from '../../../comments/interfaces/comments.repository';
 import BlogUserBanRepository from '../../interfaces/blog.user.ban.repository';
 import BlogsRepository from '../../interfaces/blogs.repository';
-import { BlogError } from '../../models/blog.error';
 import BlogUserBanCommand from '../commands/blog.user.ban.command';
 import { BlogUserBan } from '../../typeorm/models/blog.user.ban';
 import { Blog } from '../../typeorm/models/blog';
 import { User } from '../../../../users/typeorm/models/user';
 import UsersRepository from '../../../../users/interfaces/users.repository';
+import {
+  BadRequestException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
 
 @CommandHandler(BlogUserBanCommand)
 export class BlogUserBanHandler implements ICommandHandler<BlogUserBanCommand> {
@@ -18,14 +22,15 @@ export class BlogUserBanHandler implements ICommandHandler<BlogUserBanCommand> {
     private readonly usersRepo: UsersRepository,
   ) { }
 
-  async execute(command: BlogUserBanCommand): Promise<BlogError> {
+  async execute(command: BlogUserBanCommand): Promise<void> {
     const { blogId, bloggerId, isBanned, userId, banReason } = command.data;
     const blog = await this.blogRepo.get(blogId);
-    if (!blog) return BlogError.NotFound;
-    if (blog.ownerId !== bloggerId) return BlogError.Forbidden;
+    if (!blog) throw new NotFoundException('blog not found');
+    if (blog.ownerId !== bloggerId)
+      throw new ForbiddenException('operation not allowed');
 
     const user = await this.usersRepo.get(userId);
-    if (!user) return BlogError.NotFound;
+    if (!user) throw new NotFoundException('user not found');
 
     if (isBanned) return this.create(banReason, blog, user);
     return this.delete(blogId, userId);
@@ -35,18 +40,18 @@ export class BlogUserBanHandler implements ICommandHandler<BlogUserBanCommand> {
     banReason: string,
     blog: Blog,
     user: User,
-  ): Promise<BlogError> {
+  ): Promise<void> {
     const alreadyCreated = await this.banRepo.get(blog.id, user.id);
-    if (alreadyCreated) return BlogError.NoError;
+    if (alreadyCreated) return;
 
     const ban = BlogUserBan.create(banReason, blog, user);
     const result = await this.banRepo.create(ban);
     await this.commentRepo.banByBlogger(user.id, blog.id, true);
-    return result ? BlogError.NoError : BlogError.Unknown;
+    if (!result) throw new BadRequestException('cannot create ban');
   }
-  private async delete(blogId: string, userId: string): Promise<BlogError> {
-    await this.banRepo.delete(blogId, userId);
+  private async delete(blogId: string, userId: string): Promise<void> {
+    const result = await this.banRepo.delete(blogId, userId);
     await this.commentRepo.banByBlogger(userId, blogId, false);
-    return BlogError.NoError;
+    if (!result) throw new BadRequestException('cannot remove ban');
   }
 }
